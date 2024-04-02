0b73f04be5717261e9343a5539f89f0c9302e776
aayushsiwa <22052177@kiit.ac.in>
2024-04-02 05:44:42 +0430

    done

---
+++ src/temp/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <section id="projects" className="project">
+      <div className="container">
+        <h2 className="new-projects">New projects coming soon.. 🤠</h2>
+      </div>
+    </section>
+
+  )
+}
