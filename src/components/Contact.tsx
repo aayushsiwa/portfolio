be63a2cd2ffd4a548f7866620c4e8a4d066e08b7
Aayush Siwach <22052177@kiit.ac.in>
2024-08-13 22:23:20 +0530

    added resume link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -16,26 +16,29 @@ export default function Contact() {
                     </div>
                     <div className="flex gap-32 flex-wrap mt-24">
                         <div className="flex items-center gap-6">
+                        <a href="https://maps.app.goo.gl/uAToS3VVq35UnpTc8" className="hover:scale-110">
                             <span className="flex bg-white w-20 items-center justify-center h-20 shadow-md rounded-[50%] text-[#147efb]">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width={30}
-                                    height={30}
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="tabler-icon tabler-icon-map-search"
-                                >
-                                    <path d="M11 18l-2 -1l-6 3v-13l6 -3l6 3l6 -3v7.5" />
-                                    <path d="M9 4v13" />
-                                    <path d="M15 7v5" />
-                                    <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                                    <path d="M20.2 20.2l1.8 1.8" />
-                                </svg>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width={30}
+                                        height={30}
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="tabler-icon tabler-icon-map-search"
+                                    >
+                                        <path d="M11 18l-2 -1l-6 3v-13l6 -3l6 3l6 -3v7.5" />
+                                        <path d="M9 4v13" />
+                                        <path d="M15 7v5" />
+                                        <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                        <path d="M20.2 20.2l1.8 1.8" />
+                                    </svg>
                             </span>
+                            </a>
+
                             <div className="flex flex-col gap-2">
                                 <h3 className="text-[1.7rem] text-[#2d2e32]">
                                     Location
