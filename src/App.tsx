27f82ffd200f23d0bab5f025428f77c67d74c147
Aayush Siwach <22052177@kiit.ac.in>
2024-11-09 02:37:49 +0530

    remove errors for dep

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 // import viteLogo from "/vite.svg";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import NavBar from "./components/NavBar";
