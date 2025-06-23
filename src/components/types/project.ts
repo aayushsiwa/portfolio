5a8509b09ccceacdb2ccebffe9f272c6b76413cc
aayushsiwa <22052177@kiit.ac.in>
2025-06-24 00:44:46 +0530

    upgraded to tw v4
    now try to focus on improvement of mobile view and imrpovement of ui components

---
+++ src/components/types/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  title: string;
+  description: string;
+  imgSrc: string;
+  githubLink: string;
+  liveLink: string;
+  tags?: string[];
+  category?: string;
+  featured?: boolean;
+}
