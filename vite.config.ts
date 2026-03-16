81c12ad598079166c0601958a0e929f63b66558f
aayushsiwa <22052177@kiit.ac.in>
2026-03-16 18:10:57 +0530

    try dep with changes to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import vinext from "vinext";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  define: {
+    "process.env.NEXT_PUBLIC_SUPABASE_URL": JSON.stringify(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+    ),
+    "process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY": JSON.stringify(
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    ),
+  },
+
   plugins: [
     vinext(),
     cloudflare({
